a5241f8203e1565eb571353b80a6c9fce2d30753
kamilksi <kamilksiazek002@gmail.com>
2023-02-13 21:28:58 +0100

    Add page

--- lib/presentation/add_page/add_page.dart
+++ lib/presentation/add_page/add_page.dart
@@ -1,11 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/injectable/injectable.dart';
+import 'package:todo_app/presentation/add_page/cubit/add_page_cubit.dart';
+import 'package:todo_app/presentation/add_page/widgets/add_page_body.dart';
+
+const _title = Text("Dodaj zadanie");
 
 class AddPage extends StatelessWidget {
   const AddPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: _title,
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: BlocProvider(
+          create: (_) => getIt<AddPageCubit>(),
+          child: const AddPageBody(),
+        ),
+      );
 }
---
+++ lib/presentation/add_page/widgets/add_page_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class AddPageBody extends HookWidget {
+  const AddPageBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

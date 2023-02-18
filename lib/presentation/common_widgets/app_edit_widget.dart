9cc277f782617ea66ccf2579b05e4d771cdc72d9
kamilksi <kamilksiazek002@gmail.com>
2023-02-18 12:35:41 +0100

    Reusable widget for Add and edit

---
+++ lib/presentation/common_widgets/app_edit_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/presentation/add_page/cubit/add_page_state.dart';
+import 'package:todo_app/presentation/common_widgets/app_text_field.dart';
+
+import 'app_elevated_button.dart';
+
+class AppEditWidget extends StatelessWidget {
+  const AppEditWidget({
+    required this.onPressed,
+    required this.text,
+    this.width,
+    required this.titleController,
+    required this.descriptionController,
+    this.addState,
+  });
+
+  final VoidCallback? onPressed;
+  final String text;
+  final double? width;
+  final AddPageState? addState;
+  // final EditPageState? editState;
+  final TextEditingController titleController;
+  final TextEditingController descriptionController;
+  @override
+  Widget build(BuildContext context) => SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+                child: Form(
+                  child: Column(
+                    children: <Widget>[
+                      AppTextField(
+                          titleController: titleController,
+                          descriptionController: descriptionController),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      AppElevatedButton(
+                        onPressed: onPressed,
+                        text: text,
+                        width: width,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+}
